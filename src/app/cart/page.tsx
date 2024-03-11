a1a2005ccbcf58a29ff1517666ad547c770da7a2
vduplexy <timba.gc.1784@gmail.com>
2024-03-11 19:05:48 +0800

    Updated Cart to show prices in PHP

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -13,7 +13,7 @@ const CartPage = () => {
             <h1 className="uppercase text-xl font-bold">sicilian</h1>
             <span>Large</span>
           </div>
-          <h2 className="font-bold">$79.90</h2>
+          <h2 className="font-bold">₱618</h2>
           <span className="cursor-pointer">X</span>
         </div>
         {/* SINGLE ITEM */}
@@ -23,7 +23,7 @@ const CartPage = () => {
             <h1 className="uppercase text-xl font-bold">sicilian</h1>
             <span>Large</span>
           </div>
-          <h2 className="font-bold">$79.90</h2>
+          <h2 className="font-bold">₱618</h2>
           <span className="cursor-pointer">X</span>
         </div>
         {/* SINGLE ITEM */}
@@ -33,7 +33,7 @@ const CartPage = () => {
             <h1 className="uppercase text-xl font-bold">sicilian</h1>
             <span>Large</span>
           </div>
-          <h2 className="font-bold">$79.90</h2>
+          <h2 className="font-bold">₱618</h2>
           <span className="cursor-pointer">X</span>
         </div>
       </div>
@@ -41,7 +41,7 @@ const CartPage = () => {
       <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
         <div className="flex justify-between">
           <span className="">Subtotal (3 items)</span>
-          <span className="">$81.70</span>
+          <span className="">₱1854</span>
         </div>
         <div className="flex justify-between">
           <span className="">Service Cost</span>
@@ -49,12 +49,12 @@ const CartPage = () => {
         </div>
         <div className="flex justify-between">
           <span className="">Delivery Cost</span>
-          <span className="text-green-500">FREE!</span>
+          <span className="">₱75</span>
         </div>
         <hr className="my-2" />
         <div className="flex justify-between">
           <span className="">TOTAL(INCL. VAT)</span>
-          <span className="font-bold">$81.70</span>
+          <span className="font-bold">₱1854</span>
         </div>
         <button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end">
           CHECKOUT
