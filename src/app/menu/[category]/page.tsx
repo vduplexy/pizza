da1f188d8bb3371a2ff3f3bd70877c595fe841be
vduplexy <timba.gc.1784@gmail.com>
2024-03-11 18:12:59 +0800

    Changed the Store to use PHP as currency entirely

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -17,7 +17,7 @@ const CategoryPage = () => {
           {/* TEXT CONTAINER */}
           <div className="flex items-center justify-between font-bold">
             <h1 className="text-2xl uppercase p-2">{item.title}</h1>
-            <h2 className="group-hover:hidden text-xl">${item.price}</h2>
+            <h2 className="group-hover:hidden text-xl">₱{item.price}</h2>
             <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">Add to Cart</button>
           </div>
         </Link>
