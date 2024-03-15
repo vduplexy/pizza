af8e011ae8726c3958064af87a06230aa230afa7
Ubuntu <ubuntu@ip-172-31-36-244.ap-southeast-1.compute.internal>
2024-03-15 11:52:52 +0000

    Added GET API request to get Product Categories

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,9 +1,25 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
-    return new NextResponse("Hello", { status: 200});
+const prisma = new PrismaClient()
+
+export const GET = async () => {
+    try {
+        const categories = await prisma.category.findMany();
+        return new NextResponse(
+            JSON.stringify(categories),
+            { status: 200 }
+        );
+    } catch (err) {
+        console.log(err);
+        return new NextResponse(
+            JSON.stringify({ message: "Somethings went wrong!" }),
+            { status: 500 }
+        );
+    }
+
 };
 
 export const POST = () => {
-    return new NextResponse("Hello", {status: 200});
+    return new NextResponse("Hello", { status: 200 });
 }
